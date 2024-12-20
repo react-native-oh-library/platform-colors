ae9c96cfabe98b4edfcb16a3eb2c162bc868021f
Qy742698 <155599655+Qy742698@users.noreply.github.com>
2024-12-20 15:59:53 +0800

    chore: add README.OpenSource and OAT.XML and COMMITTERS.md (#7)

--- src/templates/harmony.spec.js
+++ src/templates/harmony.spec.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 const generate = require('./harmony');
 const parseColorManifest = require('@klarna/platform-colors/src/parse-color-manifest');
 const lightColors = parseColorManifest(require('@klarna/platform-colors/src/templates/fixtures/light-colors.json'));
